79faebcc12128595ad6265105b5adf9835f6d886
Aryan-Motwani <81680234+Aryan-Motwani@users.noreply.github.com>
2024-04-30 16:15:07 +0530

    finalize

--- src/Form.js
+++ src/Form.js
@@ -49,8 +49,8 @@ function Form() {
             required
           />
           <button>Submit</button>
-          {`width = ${window.innerWidth} height = ${window.innerHeight} `}
-          {}
+          {/* {`width = ${window.innerWidth} height = ${window.innerHeight} `} */}
+          {/* {} */}
         </form>
       </div>
     </div>
